8bcf42963e18a133026c3d793c6e209090dedc45
Bruna Aparecida <Bruna@MACECOC02V57EVH.local>
2022-11-20 13:25:46 -0300

    Apostila4(POO) Ex 1.3

--- Impressora.java
+++ Impressora.java
@@ -7,5 +7,8 @@ public class Impressora {
     public float exibir (float a, float b){
         return a + b;
     }
+    public float exibir (float a, String b){
+        return a + Integer.parseInt(b);
+    }
     
 }
